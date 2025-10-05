d95b09cd8ab7b2ace784cdb586376d2bd7b7862e
sumin <smkl1004@snu.ac.kr>
2025-10-05 17:35:14 +0900

    vercel-레포 연결 디버깅

--- src/components/KakaoMap.jsx
+++ src/components/KakaoMap.jsx
@@ -81,5 +81,10 @@ export default function KakaoMap() {
     }
   }, []);
 
-  return <div id="map" style={{ width: "100%", height: "100vh" }} />;
+  return (
+    <>
+      <div id="map" style={{ width: "100%", height: "100vh" }} />
+      <p>카카오맵</p>
+    </>
+  );
 }
